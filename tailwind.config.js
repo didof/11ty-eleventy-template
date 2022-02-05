5bf7ac6b62287dcdb499f54d409b2a73757a75da
didof <didonato.fr@gmail.com>
2022-02-05 17:43:59 +0100

    add netlify attribute on form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 module.exports = {
   mode: 'jit',
   media: false,
-  content: ["./src/**/*.{md,html,njk,js}"],
+  content: ['./src/**/*.{md,html,njk,js}'],
   theme: {
+    colors: {
+      white: "#fff",
+      blue: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
